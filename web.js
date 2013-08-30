a05449ab5bb8d9f5d5cfcd938dac793b3238b7a1
Todd Burner <todd.burner@gmail.com>
2013-08-30 16:52:17 +0000

    Update web.js

--- web.js
+++ web.js
@@ -1,12 +1,10 @@
+var fs = require('fs');
+var readFile = fs.readFileSync('index.html');
 var express = require('express');
-var app = express();
-app.use(express.logger());
-
+var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+    //  response.send('Hello World2!');
+   response.send(readFile.toString());});
+var port = process.env.PORT || 8080;app.listen(port, function() {  
+console.log("Listening on " + port);
 });
