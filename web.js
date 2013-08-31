d70620e28e50530ed6bf1defc2e8621d5ca8d082
Todd Burner <todd.burner@gmail.com>
2013-08-31 03:16:10 +0000

    restyled bitstarter

--- web.js
+++ web.js
@@ -1,10 +1,15 @@
-var fs = require('fs');
-var readFile = fs.readFileSync('index.html');
 var express = require('express');
+var fs = require('fs');
+var htmlfile = "index.html";
+
 var app = express.createServer(express.logger());
+
 app.get('/', function(request, response) {
-    //  response.send('Hello World2!');
-   response.send(readFile.toString());});
-var port = process.env.PORT || 8080;app.listen(port, function() {  
-console.log("Listening on " + port);
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
